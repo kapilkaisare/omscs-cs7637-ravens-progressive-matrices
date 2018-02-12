e031a57892f4e53fad080a526008e1a396499892
Kapil Kaisare <kapil@kaisare.in>
2018-02-12 15:16:21 -0600

    [p2] Start a semantic network solver

--- helpers/submit.sh
+++ helpers/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-files="$(find ./rpm_solver -type f | paste -sd " " -)"
+files="$(find ./rpm_solver -type f -name "*.py" | paste -sd " " -)"
 python submit.py --provider gt --assignment $1 --files $files
--- rpm_solver/rpm_solver.py
+++ rpm_solver/rpm_solver.py
@@ -1,7 +1,41 @@
+"""
+    rpm_solver.py
+
+    1. Provides AgentType, which showcases the different solvers available.
+    2. Defines RPMSolver, the facade for all solvers
+"""
+
+from .verbal.semantic_networks.semantic_network_solver import SNSolver
+
+"""
+    AgentType
+
+    At the time of this writing, only SIMONE is planned.
+"""
+class AgentType(object):
+    SIMONE = 1  # Uses simple semantic networks
+
+
+"""
+    NoAgentException, for when no agent is provided.
+"""
+class NoAgentException(Exception):
+    pass
+
+
+"""
+    RPMSolver:
+
+    The facade through which other solvers are invoked and put to work.
+"""
 class RPMSolver(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, agent_type):
+        self.agent = None
+        if agent_type == AgentType.SIMONE:
+            self.agent = SNSolver()
+        else:
+            raise NoAgentException()
 
     def solve(self, problem):
-        return -1
+        return self.agent.solve(problem)
