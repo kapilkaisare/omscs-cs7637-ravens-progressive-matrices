71dba4efd7ff3702c3e77f13ec8972d75581b404
Kapil Kaisare <kapil@kaisare.in>
2018-02-12 18:51:55 -0600

    [p2] Define Attributes and Attribute Changes

---
+++ rpm_solver/verbal/common/exceptions.py
@@ -0,0 +1,17 @@
+class UnknownShapeException(Exception):
+    pass
+
+class UnknownSizeException(Exception):
+    pass
+
+class UnknownFillException(Exception):
+    pass
+
+class BadAngleException(Exception):
+    pass
+
+class NotAListException(Exception):
+    pass
+
+class AttributeMismatchException(Exception):
+    pass
